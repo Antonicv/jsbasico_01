da3d1c6474a71418d8832b04b4a7994e7703e75b
antoni <antonicv.88@gmail.com>
2023-11-06 13:22:32 +0100

    agregando los archivos nuevos

---
+++ js_01.js
@@ -0,0 +1,74 @@
+// VARIABLES
+
+let numero1 = 1312 //variable de tipo entero
+console.log(typeof numero1)
+
+let numero2 = 1.2 //variable de tipo decimal
+console.log(typeof numero2)
+
+let texto1 = "esto es un texto"
+
+console.log (typeof texto1)
+
+let verdadero = true 
+console.log (typeof verdadero
+    )
+//document.getElementById ("js1").innerText = numero1
+console.log (numero1)
+
+// variables númericas
+
+let num1 = 1
+let num2 = 2
+let num3 = 3
+
+num1 = 300
+let suma = num1 + num2
+console.log (suma)
+let resta = num1 - num2
+console.log (resta)
+let multiplicar = num1 * num2
+console.log (multiplicar)
+
+console.log (20/2)
+// puede dividir entre 0
+console.log (1000/0)
+// raiz cuadrada
+console.log (9**0.5)
+// potencia
+console.log (9**2)
+// exponente
+console.log (27 ** (1/3))
+// modulo: igual al resto de la division
+console.log (20%3)
+
+
+console.log ("la suma es", 6511692162+511594)
+
+let total =1+2-3*4/5
+
+// strings
+
+texto1 = "buenos"
+let texto2 = "dias"
+
+
+
+texto_total = texto1 + " " + texto2
+console.log (texto_total)
+console.log (" el texto tiene", texto_total.length, "caracteres")
+console.log (texto_total.toUpperCase())
+console.log (texto_total.toLowerCase ())
+
+let nombre = "vIcTor peREZ".toLocaleLowerCase ()
+console.log (nombre)
+
+let saludo = "buenos dias"
+saludo = saludo - "dias"
+// NaN significa not a number
+console.log (saludo)
+
+let num5 = 5
+num5 = num5 +4
+
+console.log ( "num 5 vale", num5)
